6afcb8e28bae1641eefe1081f999e4832f925fc4
DDale21 <christiandalepancho@gmail.com>
2025-02-19 20:05:07 +1300

    Client: Added axios.get to test "GET api/v1/orders" endpoint. Added cors to prevent policy error and allow API requests from client.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import orderRoutes from "./routes/orderRoutes.js"
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use("/api/v1/orders", orderRoutes);
 
 const port = process.env.PORT || 3000;
