5725df5c06201e910c01f61e923212199bef0c82
DDale21 <christiandalepancho@gmail.com>
2025-03-24 00:19:03 +1300

    Client: Test retrieving of order. Render order details.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,42 @@ import axios from "axios"
 import './App.css'
 
 function App() {
-  const [data, setData] = useState("");
+  const [order, setOrder] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/v1/orders");
-        setData(JSON.stringify(response.data));
+        const orderId = "67dfeb1956c79fc431717398";
+        const response = await axios.get(`http://localhost:3000/api/v1/orders/${orderId}`);
+        console.log(response.data.order)
+        setOrder(response.data.order);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
   }, []);
+  
 
   return (
     <>
-      <div>{data}</div>
+      <h1>Order Details</h1>
+      {order ? (
+        <div>
+          <p><strong>Order ID: </strong>{order._id}</p>
+          <p><strong>Items</strong></p>
+          {order.pizzas.map(pizza => {
+            return (
+            <>
+              <p><strong>Pizza: </strong>{pizza.name}</p>
+              <p><strong>Size: </strong>{pizza.size}</p>
+              <p><strong>Quantity: </strong>{pizza.quantity}</p>
+            </>
+          )})}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
 }
